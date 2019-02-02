619af4c2285a54b899109bdddfc7150bc8635d4e
Ali Alsayed <a_alsayed87@hotmail.com>
2019-02-02 07:46:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,11 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/account'
+    else
+      redirect'/failure
   end
 
   get "/failure" do
